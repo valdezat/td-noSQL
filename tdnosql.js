33e55c4dab1062cf7bcb479b082b743629cafe77
Moi <valentin.dezat@gmail.com>
2020-03-23 11:22:50 +0100

    td v1

---
+++ tdnosql.js
@@ -0,0 +1,17 @@
+db.createCollection("sport")
+
+db.sport.insertOne({name : "Football", creation : 1880, surface : "gazon"})
+
+db.sport.insertOne({name : "Tennis", creation : 1870, surface : "dur"})
+
+db.sport.insertOne({name : "Rugby", creation : 1910, surface : "gazon"})
+
+db.sport.updateMany({}, {$set : {title :"", requiredTeams: true}}, {upsert : true})
+
+db.sport.updateMany({requiredTeams : true}, {$set :{minimumJoueurs :0}})
+
+db.sport.updateMany({requiredTeams:true}, {$inc:{minimumJoueurs : 10}})
+
+db.sport.updateMany({}, {$push : {title :[]}}, {upsert : true})
+
+db.sport.updateMany({requiredTeams : true}, {$push : {title :[]}}, {upsert : true})
